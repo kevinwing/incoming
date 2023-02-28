0430fbb32554c39917cfcd890e863a35b51665e9
connor-mcelroy <cdmcelroy@nic.edu>
2023-02-27 16:27:51 -0800

    added AI cover navigation

--- Assets/src/Connor/AI_square.cs
+++ Assets/src/Connor/AI_square.cs
@@ -5,56 +5,93 @@ using UnityEngine.AI;
 
 public class AI_square : MonoBehaviour
 {
-    //public player and speed
-    public GameObject player;
-    public float speed;
+    //public cover object array and tracker
+    public GameObject coverPoint;
+    private GameObject[] all = {};
+    private int index;
 
-    //private distance float
-    private float distance;
+    //collider
+    public Collider2D coll;
 
+    //dodgeball ref
     public GameObject ball;
+
+    //nav mesh ref
+    NavMeshAgent agent;
+
+    //target vector3
+    private Vector3 target;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnBall());
+        //establish navigation
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        //establish first random cover object
+        all = GameObject.FindGameObjectsWithTag("cover");
+        index = (int)Random.Range(0, all.Length - 1);
+        coverPoint = all[index];
+
+        //StartCoroutine(spawnBall());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //determine the distance and direction of the player
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
-        direction.Normalize();
+        //determine the target cover point location
+        target = coverPoint.transform.position;
+
+        //set target
+        setPos();
 
         //function I found to get the AI to face the cover its moving to
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        //move
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(Vector3.forward * angle);
-
-        //pick new cover if made it
-        if (this.transform.position == player.transform.position)
-        {
-            reCover();
-        }
-
     }
 
+    //trigger recover
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        reCover();
+    }
+
+    //set nav mesh destination
+    private void setPos()
+    {
+        agent.SetDestination(new Vector3(target.x, target.y, transform.position.z));
+    }
+
+    //find new cover
     private void reCover()
     {
-        //temp array for all cover objects
-        GameObject[] all = GameObject.FindGameObjectsWithTag("cover");
-        int length = all.Length;
+        //if topped out, reset to 0, else randomly pick forward or back
+        if (index == all.Length - 1)
+        {
+            index = (int)Random.Range(0, all.Length - 1);
+        }
+        else
+        {
+            if ((Random.value > 0.5f))
+            {
+                index++;
+            }
+            else
+            {
+                if (index == 0)
+                    index = all.Length - 1;
+                else
+                    index--;
+            }
+        }
 
-        //assign new random cover
-        player = all[(int)Random.Range(0, length)];
-
+        //assign new cover
+        coverPoint = all[index];
     }
 
-    //spawn new barrel
+    //spawn new ball
     IEnumerator spawnBall()
     {
         while (true)
