d085628b7f052335bf3bf29d80ccf5392c4b65a3
Kevin Wing <kevinrwing@gmail.com>
2023-03-22 14:02:12 -0700

    Updated HealthTest.cs(Still not working)
    This is not working, but from my research it should. It gives an error saying it is an 'Editor' script and cannot be inside the editor folder. It is not inside the editor folder

--- Assets/tst/Kevin/TestsKevin/HealthTest.cs
+++ Assets/tst/Kevin/TestsKevin/HealthTest.cs
@@ -4,57 +4,41 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class HealthTest : MonoBehaviour
+public class HealthTest
 {
     [Test]
-    public IEnumerator playerTakes99Damage()
+    public void playerTakes99Damage()
+    {
+        var obj = new GameObject(); // create empty game object
+        var player = obj.AddComponent<Player>(); // add Player object to obj and get reference
+        player.setHealth(100); // ensure player object health is at 100
+
+        player.TakeDamage(99); // do less than 100 damage
+
+        Assert.AreEqual(1, player.getHealth()); // should have 1 hitpoint left
+    }
+
+    [Test]
+    public void playerTakesFatalHitHealthCheck()
     {
         var obj = new GameObject();
         var player = obj.AddComponent<Player>();
         player.setHealth(100);
-        player.TakeDamage(99);
 
-        Assert.AreEqual(1, player.getHealth());
-        yield return null;
-    }
-
-    [Test]
-    public IEnumerator playerTakes101DamageAndDies()
-    {
-        GameObject obj = new GameObject();
-        obj.AddComponent<Player>();
-        Player player = obj.GetComponent<Player>();
-        player.setHealth(100);
-        player.TakeDamage(101);
-
-
-        Assert.AreEqual(-1, player.getHealth());
-        yield return null;
-    }
-
-    [Test]
-    public IEnumerator playerTakes100DamageAndDies()
-    {
-        GameObject obj = new GameObject();
-        obj.AddComponent<Player>();
-        Player player = obj.GetComponent<Player>();
-        player.setHealth(100);
         player.TakeDamage(100);
 
         Assert.AreEqual(0, player.getHealth());
-        yield return null;
     }
 
         [Test]
-    public IEnumerator checkPlayerIsDead()
+    public void playerTakesFatalHitIsDead()
     {
-        GameObject obj = new GameObject();
-        obj.AddComponent<Player>();
-        Player player = obj.GetComponent<Player>();
+        var obj = new GameObject();
+        var player = obj.AddComponent<Player>();
         player.setHealth(100);
+
         player.TakeDamage(100);
 
         Assert.AreEqual(false, player.isDead());
-        yield return null;
     }
 }
