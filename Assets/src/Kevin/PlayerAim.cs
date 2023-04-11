589c680052e613aa3fdecf38cb939a33fd9fae8a
Kevin Wing <kevinrwing@gmail.com>
2023-04-10 23:37:55 -0700

    Added ball floating around player

---
+++ Assets/src/Kevin/PlayerAim.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    private Transform aimTransform;
+    private Transform ballTransform;
+    public GameObject Ball;
+    private Vector3 ballPosition;
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+        ballTransform = aimTransform.Find("Ball");
+    }
+
+    private void Update()
+    {
+        HandleAiming();
+    }
+
+    private void HandleAiming()
+    {
+        Vector3 mousePosition = GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private void HandleShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePostition = GetMouseWorldPosition();
+            ballPosition = ballTransform.position;
+
+            GameObject newBall = Instantiate(Ball, ballPosition, Quaternion.identity);
+            // newBall.setTarget()
+        }
+    }
+
+    private static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+
+    private static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+
+    private static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+
+    private static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPostion, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPostion);
+        return worldPosition;
+    }
+}
