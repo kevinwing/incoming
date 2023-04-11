2218a0fc2b9f8812f9f69834276c71b3adbc0d90
Gary Banks <34329789+AVLnCS@users.noreply.github.com>
2023-04-11 16:52:38 -0700

    Added basic pause menu, moved audio volume slider to it.

---
+++ Assets/src/Gary/pauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject pauseButton;
+    public GameObject helpMenu;
+    public GameObject healthBar;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)){
+            if(isPaused){
+                Resume();
+            }
+            else{
+                Pause();
+            }
+        }
+    }
+
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    public void Pause(){
+        pauseMenuUI.SetActive(true);
+        pauseButton.SetActive(false);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void Quit(){
+        Application.Quit();
+    }
+    
+
+    public void Help(){
+        helpMenu.SetActive(true);
+        pauseMenuUI.SetActive(false);
+        healthBar.SetActive(false);
+    }
+    public void Return(){
+        helpMenu.SetActive(false);
+        pauseMenuUI.SetActive(true);
+        healthBar.SetActive(true);
+    }
+}
