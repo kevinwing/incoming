9d7bf71cc2deabcfaa5f4e0e21bdac113a853769
Kevin Wing <kevinrwing@gmail.com>
2023-04-12 23:16:11 -0700

    Copied tilemap to lvl1

---
+++ Assets/src/Kevin/LevelManager/EnemyPool.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPool : MonoBehaviour
+{
+    private Queue<GameObject> _enemyQueue; // instantiate a queue to hold enemies
+
+    
+}
--- Assets/src/Kevin/LevelManager/LevelSpawner.cs
+++ Assets/src/Kevin/LevelManager/LevelSpawner.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class LevelSpawner : MonoBehaviour
 {
-    [SerializeField] private EnemyPool enemyPool;
+    private ObjectPool<GameObject> enemyPool;
+
+    private GameObject[] spawnPoints;
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private GameObject bossPrefab;
+    [SerializeField] private GameObject ballPrefab;
+
+    // Enemy prefabs
+    [SerializeField] private GameObject enemyPrefab1;
+    // [SerializeField] private GameObject enemyPrefab2;
+    // [SerializeField] private GameObject enemyPrefab3;
 
     private void Start()
     {
-        
+        // playerPickupBall = GameObject.Find("Player").GetComponent<PlayerPickupBall>();
+        spawnPoints = GameObject.FindGameObjectsWithTag("spawn_point"); // get array of spawn points
     }
 }
