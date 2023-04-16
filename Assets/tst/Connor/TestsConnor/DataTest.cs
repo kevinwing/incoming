ce0544a06e593e1f36cc62705a0e3e10fe08304c
connor-mcelroy <cdmcelroy@nic.edu>
2023-04-15 22:29:05 -0700

    added data struct test

---
+++ Assets/tst/Connor/TestsConnor/DataTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class DataTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void DataStructTest()
+    {
+        //Arrange
+        var expected = 10;
+        var gObject = new GameObject();
+        var AI = gObject.AddComponent<AI_square>();
+        AI.Awake();
+
+        //ACT
+        var aiData = AI.GetAIData();
+
+
+        //Assert
+        Assert.IsNotNull(aiData);
+    }
+}
