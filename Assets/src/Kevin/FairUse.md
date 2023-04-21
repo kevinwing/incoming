4c48c1f3994b858e7d8186da5c084bc612d4f23b
Kevin Wing <kevinrwing@gmail.com>
2023-04-21 01:12:43 -0700

    Added some notes

---
+++ Assets/src/Kevin/FairUse.md
@@ -0,0 +1,12 @@
+There are certain circumstances where the use of a digital asset without attribution could be considered fair use under US copyright law. However, fair use is a complex and often subjective legal concept, and each case will depend on the specific facts and circumstances involved.
+
+In general, fair use is a legal doctrine that allows the limited use of copyrighted material without the permission of the copyright owner. To determine whether a particular use is fair use, courts typically consider four factors:
+
+- The purpose and character of the use
+- The nature of the copyrighted work
+- The amount and substantiality of the portion used in relation to the whole
+- The effect of the use on the potential market for or value of the copyrighted work.
+
+While it is possible that a particular use without attribution could be considered fair use under certain circumstances, it is important to note that fair use is not a blanket exception to copyright law. It is a nuanced and fact-specific analysis, and the outcome of any given fair use case will depend on the specific facts and circumstances involved.
+
+In general, it is always best to err on the side of caution and provide proper attribution when using digital assets to avoid any potential legal issues.
--- Assets/src/Kevin/Player/Player.cs
+++ Assets/src/Kevin/Player/Player.cs
@@ -3,9 +3,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/* virtual/override
+In C#, a virtual function is a method that can be overridden by a derived class. When a virtual method is called on an object,
+the runtime determines which implementation of the method to use based on the actual type of the object.
+
+To define a virtual function in C#, you can use the "virtual" keyword in the method declaration. For example:
+
+public virtual void MyVirtualMethod()
+{
+    // Method implementation
+}
+
+To override a virtual method in a derived class, you can use the "override" keyword in the method declaration. For example:
+
+public override void MyVirtualMethod()
+{
+    // Method implementation for the derived class
+}
+
+By using virtual functions, you can create a base class with default implementations of methods that can be customized by derived classes.
+This enables polymorphic behavior, where a single interface can be used to interact with objects of different types, each with their own
+implementation of the virtual method.
+*/
+
+/* Dynamic vs. Static Binding
+In C#, binding refers to the process of connecting a method call to the method implementation.
+There are two types of binding: dynamic binding and static binding.
+
+Static binding, also known as early binding, occurs when the method call is resolved at compile-time.
+The compiler determines which method to call based on the type of the object reference. The method call
+is bound to the method implementation at compile-time and cannot be changed at runtime.
+
+Dynamic binding, also known as late binding, occurs when the method call is resolved at runtime. The method
+call is bound to the method implementation based on the actual type of the object at runtime. This allows
+for greater flexibility as the method implementation can be changed at runtime.
+
+In C#, static binding is the default behavior. However, you can use the "dynamic" keyword to enable dynamic
+binding for method calls. Dynamic binding is commonly used in scenarios where the exact method implementation
+is not known until runtime, such as with plugin architectures or when working with external APIs.
+*/
+
 public class Player : MonoBehaviour
 {
-
     [SerializeField] private float _speed;
     private int _health = 100;
     private bool is_dead = false;
